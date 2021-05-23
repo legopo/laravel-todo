5cce3c4af206e22ff8787e8cc8e51c26d4d10ab0
legopo <taraki3594@aol.jp>
2021-05-23 20:50:43 +0900

    #7 mvcのベース + ルーティング準備

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
