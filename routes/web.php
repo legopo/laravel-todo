7d08240c19fb986a1ac3d916dff070e2d5d08ab6
legopo <taraki3594@aol.jp>
2021-07-24 13:59:28 +0900

    #16 認証機能

--- routes/web.php
+++ routes/web.php
@@ -6,30 +6,38 @@ use App\Http\Controllers\TagController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 
-// TODO: auth で囲む　Route::middleware(['first', 'second'])->group(function () { });
+Route::get('/', [TaskController::class, 'index']);
 
-// User
-Route::resource('/users', UserController::class)
-    ->only([
-        'show',
-        'edit',
-        'update',
-        'destroy'
-    ]);
+Route::middleware(['auth'])->group(function () {
+    // User
+    Route::resource('/users', UserController::class)
+        ->only([
+            'show',
+            'edit',
+            'update',
+            'destroy'
+        ]);
 
-// Group
-Route::resource('/groups', GroupController::class)
-    ->only([
-        'create',
-        'store',
-        'edit',
-        'update',
-        'destroy'
-    ]);
+    // Group
+    Route::resource('/groups', GroupController::class)
+        ->only([
+            'create',
+            'store',
+            'edit',
+            'update',
+            'destroy'
+        ]);
 
-// Task
-Route::resource('/groups/{group}/tasks', TaskController::class);
+    // Task
+    Route::resource('/groups/{group}/tasks', TaskController::class);
 
-// Tag
-Route::resource('/tag', TagController::class)
-    ->only(['store']);
+    // Tag
+    Route::resource('/tag', TagController::class)
+        ->only(['store']);
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+require __DIR__ . '/auth.php';
